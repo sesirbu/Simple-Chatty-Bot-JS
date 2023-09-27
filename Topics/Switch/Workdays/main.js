453615bbf0c515ad0f3e35303113f7b76e1f72d1
sebitaas96-ss <sesirbu2@gmail.com>
2023-09-27 18:38:41 +0200

    Simple Chatty Bot JS

---
+++ Topics/Switch/Workdays/main.js
@@ -0,0 +1,23 @@
+function checkTheDate(value){
+    switch (value) {
+      // write your code here
+        case 1:
+            console.log("Yes, you should go to work");
+            break;
+        case 2:
+            console.log("Yes, you should go to work");
+            break;
+        case 3:
+            console.log("Yes, you should go to work");
+            break;
+        case 4:
+            console.log("Yes, you should go to work");
+            break;
+        case 5:
+            console.log("Yes, you should go to work");
+            break;
+        default:
+            console.log("No, this is your well-deserved weekend!");
+            break;
+	}
+}
